17190fcde14f962269794b2234be1c6c17c57435
RamyaAnandan <ra9207@srmist.edu.in>
2026-03-18 14:48:56 +0530

    dev branch to merge all the branch

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,30 @@
+/**
+ *
+ * MAIN CLASS HotelBookingApp
+ *
+ * Use Case 1: Application Entry & Welcome Message
+ *
+ * Description:
+ * This class represents the entry point of the
+ * Hotel Booking Management System.
+ *
+ * @author RamyaAnandan
+ * @version 1.0
+ */
+
+public class BookMyStayApp {
+
+    /**
+     * Application entry point.
+     * This method is executed first by the JVM.
+     *
+     * @param args Command-line arguments
+     */
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("System initialized successfully.");
+
+    }
+}
